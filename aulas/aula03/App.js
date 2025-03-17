d9fc66fca03e86ea87680de5e1f85405fcf23332
Henrique Lobo <henriqueabreulobo@hotmail.com>
2025-03-17 20:29:16 -0300

    adicionando aula03

---
+++ aulas/aula03/App.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import RegistrarScreen from './screens/RegistrarScreen';
+
+function App() {
+  return(
+    <RegistrarScreen/>
+  )
+}
+
+export default App
